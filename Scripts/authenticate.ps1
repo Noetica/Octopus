fe7692e481e607360eb2208466b0ffa4bf5c8db9
Ian Harrison <ian.harrisonl@noetica.com>
2025-02-19 16:35:12 +0000

    fix secret

--- Scripts/authenticate.ps1
+++ Scripts/authenticate.ps1
@@ -1,8 +1,8 @@
 $tenant = "822ef784-771f-4257-9898-e94f265e7eb6"
 $clientId = "39040e89-a91c-42e3-ac10-67d8669ff1b9"
-$clientSecret = "Zfw8Q~wxFHdeH6~YMuAsDDpv8X7HHePP8-5WIdoX"
+$clientS = "Zfw8Q~wxFHdeH6~YMuAsDDpv8X7HHePP8-5WIdoX"
 
-$securePassword = ConvertTo-SecureString -String $clientSecret -AsPlainText -Force
+$securePassword = ConvertTo-SecureString -String $clientS -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $securePassword
 
 Connect-AzAccount -ServicePrincipal -TenantId $tenant -Credential $credential
