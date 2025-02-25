779b387b818c81f8f2080347bec9ad4aca460edf
J. Joseph Girardi <53053708+HeyGirardi@users.noreply.github.com>
2025-02-25 12:26:44 +0000

    Add substitutions to authenticate.ps1

--- Scripts/authenticate.ps1
+++ Scripts/authenticate.ps1
@@ -1,6 +1,6 @@
-$tenant = "822ef784-771f-4257-9898-e94f265e7eb6"
-$clientId = "39040e89-a91c-42e3-ac10-67d8669ff1b9"
-$clientS = "Zfw8Q~wxFHdeH6~YMuAsDDpv8X7HHePP8-5WIdoX"
+$tenant = "#{ServicePrincipal.TenantId}"
+$clientId = "#{ServicePrincipal.ClientId}"
+$clientS = "#{ServicePrincipal.ClientSec}"
 
 $securePassword = ConvertTo-SecureString -String $clientS -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $securePassword
