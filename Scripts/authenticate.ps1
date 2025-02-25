65c39c86bfdb16d4d0695e133dd37f2cf1db7cfa
Ian Harrison <ian.harrisonl@noetica.com>
2025-02-25 14:34:28 +0000

    make sure az connect works

--- Scripts/authenticate.ps1
+++ Scripts/authenticate.ps1
@@ -5,4 +5,11 @@ $clientS = "#{ServicePrincipal.ClientSec}"
 $securePassword = ConvertTo-SecureString -String $clientS -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $securePassword
 
+#Check if the az components are installed, and install as required
+$getCommand = Get-Command -Name New-AzApiManagementContext -ErrorAction SilentlyContinue
+
+if ($getCommand -eq $null) {
+    Install-Module -Name Az -AllowClobber -Force
+}
+
 Connect-AzAccount -ServicePrincipal -TenantId $tenant -Credential $credential
