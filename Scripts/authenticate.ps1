4e4b02a97e039a22505c16c48d273d6b8c66a07a
Ian Harrison <ian.harrisonl@noetica.com>
2025-02-26 15:39:38 +0000

    update authentication

--- Scripts/authenticate.ps1
+++ Scripts/authenticate.ps1
@@ -1,10 +1,3 @@
-$tenant = "#{ServicePrincipal.TenantId}"
-$clientId = "#{ServicePrincipal.ClientId}"
-$clientS = "#{ServicePrincipal.ClientSec}"
-
-$securePassword = ConvertTo-SecureString -String $clientS -AsPlainText -Force
-$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $securePassword
-
 #Check if the az components are installed, and install as required
 $getCommand = Get-Command -Name New-AzApiManagementContext -ErrorAction SilentlyContinue
 
@@ -12,4 +5,4 @@ if ($getCommand -eq $null) {
     Install-Module -Name Az -AllowClobber -Force
 }
 
-Connect-AzAccount -ServicePrincipal -TenantId $tenant -Credential $credential
+Connect-AzAccount -Identity
