6f64efdd97dadda43b2afad6af09cc9cdfe5874e
Ian Harrison <ian.harrisonl@noetica.com>
2025-03-10 10:16:30 +0000

    fix script

--- Scripts/run-tests.ps1
+++ Scripts/run-tests.ps1
@@ -1,7 +1,7 @@
 [Parameter(Mandatory = $true)] [string]$AppName, # Name of the artifact (versionless) or display name, e.g. ReportsAPI
 [Parameter(Mandatory = $true)] [string]$TargetDir # Location of the target deployment directory
 
-$testPath = "$TargetDir\$AppName.Tests"
+$testPath = "${$TargetDir}\${$AppName}.Tests"
 cd "$testPath"
 dotnet test --no-build
 
