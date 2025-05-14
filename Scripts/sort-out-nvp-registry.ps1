2b67ffce46a17e42536dc43aa99ff20439e190f4
LukeZHarris <luke.harris@noetica.com>
2025-05-14 11:39:01 +0100

    Remove registry updates, just scheduled the task.

--- Scripts/sort-out-nvp-registry.ps1
+++ Scripts/sort-out-nvp-registry.ps1
@@ -1,26 +1,6 @@
-# This script is designed to update the registry keys for Noetica Voice Platform
-# It does two things: 
-# 1. It updates the ExtraParameters value in all SIP trunk groups to say sipProxy=talkdeskkamailio2
-# 2. It creates a scheduled task to run the UpdateSIPdotINI.ps1 script at startup
-# That script is deployed as part of the package. 
-$parentKey = "HKLM:\Software\Noetica\Voice Platform"
-$pattern = "Group*"
-$valueName = "ExtraParameters"
-$newValue = "sipProxy=sip:talkdeskkamailio2"
-
-$subKeys = Get-ChildItem -Path $parentKey -Name | Where-Object { $_ -like $pattern }
-
-foreach ($subKey in $subKeys) {
-    $fullPath = "$parentKey\$subKey"
-    try {
-        $currentValue = Get-ItemProperty -Path $fullPath -Name $valueName -ErrorAction Stop
-        Set-ItemProperty -Path $fullPath -Name $valueName -Value $newValue
-        Write-Output "Updated $valueName in $fullPath"
-    } catch {
-        Write-Output "$valueName does not exist in $fullPath"
-    }
-}
-
+# This script installs a startup script used to configure the VM
+# It creates a scheduled task to run c:\Install\UpdateSIPdotINI.ps1 script at startup
+# The script should have been deployed first by Octopus Deploy.
 $trigger = New-ScheduledTaskTrigger -AtStartup
 $action = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument '-NoProfile -NonInteractive -WindowStyle Hidden -File "C:\install\UpdateSIPdotINI.ps1"'
 $taskName = 'Update NVP sip.ini with local IP'
