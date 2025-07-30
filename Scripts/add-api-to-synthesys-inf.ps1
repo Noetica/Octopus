071e1993b2c12fa0697288a200378be59dbcffa1
PaulXKeyes <paul.keyes@noetica.com>
2025-07-30 08:16:11 +0100

    Update add-api-to-synthesys-inf.ps1

--- Scripts/add-api-to-synthesys-inf.ps1
+++ Scripts/add-api-to-synthesys-inf.ps1
@@ -51,6 +51,9 @@ if ($match.Success) {
             Write-Host '## Saving...'
             Set-Content -Path $synthesysInf -Value $cleanedContent -Force -Encoding Ascii
             Write-Host "Saved changes to $synthesysInf"
+            Write-Host "Reloading Services"
+            Reload-Services
+            Write-Host "Reloaded Services"
         }
         Catch {
             Write-Host "[!] Failed to write changes to the file: $_"
